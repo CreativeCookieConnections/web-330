caff3cbcb5d0be5cae20cf6a380e62cccc843212
CreativeCookieConnections <creativecookieconnections@gmail.com>
2025-11-13 17:28:59 -0600

    Worked more on Week 4 chapter exercise

---
+++ week-4/chapter/js10b.js
@@ -0,0 +1,24 @@
+"use strict";
+/*  JavaScript 7th Edition
+    Chapter 10
+    Chapter case
+
+    Oak Top House Directions
+    Author: Aisha Keller
+    Date: 11/12/2025
+
+    Filename: js10b.js
+*/
+
+
+// Function to set up and display the Oak Top House Map
+function initMap() {
+
+   // Page objects
+   let displayMap = document.getElementById("displayMap");
+   let routeBox =    document.getElementById("routeBox");
+
+
+}
+
+
